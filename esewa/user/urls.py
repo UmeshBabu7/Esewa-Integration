009aa67dd7b4bbb206a55099b3bcec267f4e8bec
Umesh <umeshtamang129@gmail.com>
2025-08-05 15:07:42 +0545

    productdetail, user authentication

--- esewa/user/urls.py
+++ esewa/user/urls.py
@@ -4,5 +4,12 @@ from .import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('productpage/', views.productpage, name='productpage'),
-    path('productdetail/<int:product_id>/', views.productdetail, name='productdetail')
+    
+    # product detail
+    path('productdetail/<int:product_id>/', views.productdetail, name='productdetail'),
+
+    # user authentication
+    path('register/', views.register_user, name='register'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
--- esewa/user/views.py
+++ esewa/user/views.py
@@ -1,8 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from product.models import *
 from .filters import *
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import LoginForm
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+def register_user(request):
+    if request.method == 'POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS, 'User account has been created successfully !')
+            return redirect('login')
+        else:
+            messages.add_message(request, messages.ERROR, 'Please provide correct credential !')
+    else:
+        form=UserCreationForm()
+    return render(request, 'user/register.html', {'form':form})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form=LoginForm(request.POST)
+        if form.is_valid():
+            data=form.cleaned_data
+            user=authenticate(request, username=data['username'], password=data['password']) # read data
+
+            if user is not None:
+                login(request, user)
+                return redirect('homepage')
+            else:
+               messages.add_message(request, messages.ERROR, 'Please provide correct credential !')
+    else:
+        form=LoginForm()
+    return render(request, 'user/login.html', {'form':form})
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
 
 def homepage(request):
     product=Product.objects.all().order_by('-id')[:4]
