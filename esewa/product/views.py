caa46398a9707e2eb82026d4f31f59f4b9f2b2be
Umesh <umeshtamang129@gmail.com>
2026-01-06 15:44:05 +0545

    remove index and product page

--- esewa/product/views.py
+++ esewa/product/views.py
@@ -1,20 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Category, Product
 
 # Create your views here.
-
-
-def product(request):
-    return HttpResponse("product page")
-
-def home(request):
-    product=Product.objects.all()
-    items={
-        'product':product
-    }
-    return render(request, 'product/index.html', items)
-
-
-def productpage(request):
-    return render(request, 'product/productpage.html')
